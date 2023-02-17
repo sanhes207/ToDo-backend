9eb03c11290633454f8c7a74676a9d7c73df7557
Alexander <amoshenin@mail.ru>
2023-02-17 10:11:06 +0300

    Изменено добавление и возвращение категорий под бд

--- controller/category.controller.js
+++ controller/category.controller.js
@@ -2,21 +2,19 @@ const db = require('../db');
 
 class Category {
 	async addCategory(req, res) {
-		const {title, id_color} = req.body;
-		const newCategory = await (`
-			INSERT INTO category (title, id_color)
-			VALUES ($1, $2)
-		`, [title, id_color]);
-		res.json('Новая категория была создана');	
+		const {user_id, title} = req.body;
+		const newCategory = await db.query(`
+			INSERT INTO category (title, person_id)
+			values ($1, $2)
+		`, [title, user_id]);
+		res.json(newCategory);	
 	}
 	async getCategorys(req, res) {
 		const {user_id} = req.headers;
 		const getCategorys = await db.query(`
 			SELECT DISTINCT category.id, category.title
-			FROM category
-			INNER JOIN task
-			ON task.id_category = category.id
-			WHERE task.id_person = $1
+			FROM category	
+			WHERE person_id = $1
 		`, [user_id]);
 		res.json(getCategorys.rows);
 	}
