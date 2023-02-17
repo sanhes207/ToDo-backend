e7eada1baf9ff516fe87ecf1a2c321fc01c6f069
Alexander <amoshenin@mail.ru>
2023-02-17 10:12:24 +0300

    Изменено добавление задачи и получение задачи

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -3,55 +3,42 @@ const db = require('../db');
 class Task{
 
   async addNewTask(req, res) {
-    const {id_category, id_important, id_person, description} = req.body;
+    const {category_id, description} = req.body;
     const newTask = await db.query(`
-      INSERT INTO task (id_category, description, id_important, id_person) 
-      VALUES ($1, $2, $3, $4)
-    `, [id_category, description, id_important, id_person]);
+    INSERT INTO task (category_id, description)
+		VALUES ($1, $2)
+    `, [category_id, description]);
     res.json(newTask);
   }
   async getAllTaskByCategory(req, res) {
     const {user_id, category_id} = req.headers;
     const tasks = await db.query(`
-      SELECT task.id, task.description, category.title AS category, important.title AS important 
+      SELECT DISTINCT task.id, task.description, task.is_checked
       FROM task
-      INNER JOIN important
-      ON important.id = task.id_important
       INNER JOIN category
-      ON category.id = task.id_category
-      WHERE task.id_person = $1 AND task.id_category = $2`
-    , [user_id, category_id]);
+      ON $1 = task.category_id
+      WHERE person_id = $2
+      `, [category_id, user_id]);
     res.json(tasks.rows);
   }
   async getTask(req, res) {
     const id = req.params.id;
     const task = await db.query(`
-      SELECT task.description, category.title AS category, important.title AS important 
-      FROM task
-      INNER JOIN important
-      ON important.id = task.id_important
-      INNER JOIN category
-      ON category.id = task.id_category
-      WHERE task.id = $1`
-    , [id]);
+      
+    `, [id]);
     res.json(task.rows[0]);
   }
   async updateTask(req, res) {
     const {id_category, id_important, description, id} = req.body;
     const newTask = await db.query(`
-      UPDATE task 
-      SET id_category = $1,
-          id_important = $2,
-          decription = $3
-      WHERE task.id = $4
-      RETURNING *
+      
     `, [id_category, id_important, description, id]);
     res.json(newTask);
   }
   async deleteTask(req, res) {
-    const id = req.params.id;
-    const task = await db.query(`DELETE FROM task WHERE task.id = $1`, [id]);
-    res.json(task.rows);
+    const user_id = req.headers;
+    const task = await db.query(`DELETE FROM task WHERE task.id = $1`, [user_id]);
+    res.json('Пользователь был удален');
   }
 }
 
