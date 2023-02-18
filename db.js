ad2f1b0b17db10cf869c3a5391df81eab6673577
Alexander <amoshenin@mail.ru>
2023-02-18 09:56:29 +0300

    Database creation request moved to a separate sql file

--- db.js
+++ db.js
@@ -8,29 +8,3 @@ const pool = new Pool({
 });
 
 module.exports = pool;
-
-/*  Схема базы данных
-CREATE TABLE person (
-	id SERIAL NOT NULL,
-	name VARCHAR(255) NOT NULL,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE category (
-	id SERIAL NOT NULL,
-	title VARCHAR(255) NOT NULL,
-	color VARCHAR(7) NOT NULL DEFAULT '#FFFFFF',
-	person_id INT NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (person_id) REFERENCES person(id) 
-);
-
-CREATE TABLE task (
-	id SERIAL NOT NULL,
-	category_id INT NOT NULL,
-	description VARCHAR(255) NOT NULL,
-	is_checked BOOL DEFAULT false
-	PRIMARY KEY (id),
-	FOREIGN KEY (category_id) REFERENCES category(id) 
-)
-*/
---
+++ db.sql
@@ -0,0 +1,26 @@
+CREATE TABLE person (
+	id SERIAL NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE category (
+	id SERIAL NOT NULL,
+	title VARCHAR(255) NOT NULL,
+	color VARCHAR(7) NOT NULL DEFAULT '#FFFFFF',
+	person_id INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (person_id) REFERENCES person(id) 
+);
+
+CREATE TABLE task (
+	id SERIAL NOT NULL,
+	category_id INT NOT NULL,
+	description VARCHAR(255) NOT NULL,
+	is_checked BOOL DEFAULT false,
+	PRIMARY KEY (id),
+	FOREIGN KEY (category_id) REFERENCES category(id) 
+);
+
+INSERT INTO  person (name)
+VALUE ('user');
