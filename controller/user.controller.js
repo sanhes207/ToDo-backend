d945a46072757fd627b3648d9a3358f9023785c9
Alexander <amoshenin@mail.ru>
2023-02-21 17:36:43 +0300

    Переработал взаимодействие с запросами. Добавил обработку ошибок

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,40 +1,53 @@
 const db = require('../db');
+const query = require('../querys/user.query');
 
 class User{
+  async createUser(req, res) {
+    const {name} = req.body;
+
+    await db.query(query.addUser, [name])
+    .then(() => {
+      res.status(200).send({message: `Регистрация ${name} прошла успешно` });
+    })
+    .catch(() => {
+      res.status(500).send({message: 'Error with adding new user'});
+    })
+  }
 
   async getUser(req, res) {
     const name = req.params.name;
-    const user = await db.query(`
-      SELECT id, name 
-      FROM person
-      WHERE person.name = $1
-    `, [name]);
-    res.json(user.rows[0]);
+
+    await db.query(query.getUser, [name])
+    .then((result) => {
+      res.status(200).json(result.rows[0]);
+    })
+    .catch(() => {
+      res.status(500).send({message: 'Error with get user'})
+    })
   }
-  async createUser(req, res) {
-    const {name} = req.body;
-    const newUser = await db.query(`
-      INSERT INTO person (name)
-      VALUES ($1)
-    `, [name]);
-    res.json(`Регистрация ${name} прошла успешно`);
-  }
-  async deleteUser(req, res) {
-    const id = req.params.id;
-    const user = await db.query(`
-      DELETE FROM user WHERE user.id = $1
-    `, [id]);
-    res.json(`Пользователь удален`);
-  }
+
   async updateUser(req, res) {
     const {id, name} = req.body;
-    const user = await db.query(`
-      UPDATE person 
-      SET name = $1
-      WHERE id = $1
-      RETURNING *
-    `, [id, name]);
-    res.json(user.rows[0]);
+    
+    await db.query(query.updateUser, [id, name])
+    .then(() => {
+      res.status(200).send({message: 'User success updated'});
+    })
+    .catch(() => {
+      res.status(500).send({message: 'Error with update user'})
+    })
+  }
+
+  async deleteUser(req, res) {
+    const id = req.params.id;
+    
+    await db.query(query.deleteUSer, [id])
+    .then(() => {
+      res.status(200).send({message: 'User success deleted'});
+    })
+    .catch(() => {
+      res.status(500).send({message: 'Error with deleted user'})
+    })
   }
 }
 
