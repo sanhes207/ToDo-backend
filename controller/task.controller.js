c6d1adbfed92c70914ce9757c37f57f5874a9705
Alexander <amoshenin@mail.ru>
2023-02-21 17:23:38 +0300

    Переработал работу с запросами. Добавил обработку ошибок.

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -1,42 +1,50 @@
 const db = require('../db');
+const query = require('../querys/task.query');
 
 class Task{
 
   async addNewTask(req, res) {
     const {category_id, description} = req.body;
-    const newTask = await db.query(`
-    INSERT INTO task (category_id, description)
-		VALUES ($1, $2)
-    `, [category_id, description]);
-    res.json(newTask);
+    await db.query(query.addTask, [category_id, description])
+    .then(() => {
+      res.status(201).send({message: "New task added"});
+    })
+    .catch(() => {
+      res.status(500).send({message: "Error in process adding new task"})
+    })
   }
 
   async getAllTaskByCategory(req, res) {
     const {user_id, category_id} = req.headers;
-    const tasks = await db.query(`
-      SELECT DISTINCT task.id, task.description, task.is_checked
-      FROM task
-      INNER JOIN category
-      ON $1 = task.category_id
-      WHERE person_id = $2
-      `, [category_id, user_id]);
-    res.json(tasks.rows);
+    await db.query(query.getAllTask, [category_id, user_id])
+    .then((result) => {
+      res.status(200).json(result.rows);
+    })
+    .catch(() => {
+      res.status(500).send({message: 'Error get data'})
+    })
   }
 
   async updateTask(req, res) {
     const {is_checked, task_id} = req.body;
-    const newTask = await db.query(`
-    UPDATE task
-    SET is_checked = $1
-    WHERE task.id = $2
-    `, [is_checked, task_id]);
-    res.json(newTask);
+    const newTask = await db.query(query.updateTask, [is_checked, task_id])
+    .then(() => {
+      res.status(200).send({message: 'Task success update'});
+    })
+    .catch(() => {
+      res.status(500).send({message: 'Error update data'})
+    })
   }
 
   async deleteTask(req, res) {
     const user_id = req.headers;
-    const task = await db.query(`DELETE FROM task WHERE task.id = $1`, [user_id]);
-    res.json('Пользователь был удален');
+    const task = await db.query(query.deleteTask, [user_id])
+    .then(() => {
+      res.status(200).send({message: 'Task success delete'});
+    })
+    .catch(() => {
+      res.status(500).send({message: 'Error delete data'})
+    })
   }
 }
 
