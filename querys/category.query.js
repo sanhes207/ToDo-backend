602e1c1605323666ae57fc61f4ec17b03bc21877
Alexander <amoshenin@mail.ru>
2023-02-22 22:31:54 +0300

    Reworked query. Added in getting information about the category getting color

--- querys/category.query.js
+++ querys/category.query.js
@@ -1,7 +1,16 @@
-const insertCategory = `INSERT INTO category (title, person_id) VALUES ($1, $2)`;
-const insertCategoryWithColor = `INSERT INTO category (title, person_id, color) VALUES ($1, $2, $3)`;
-const getCategory = `SELECT DISTINCT category.id, category.title, category.color FROM category WHERE person_id = $1`;
-const updateCategory = `UPDATE category SET title = $1 id_color = $2 WHERE category.id = $3`;
+const insertCategory = `INSERT INTO category (title, person_id) 
+                        VALUES ($1, $2)`;
+const insertCategoryWithColor = `INSERT INTO category (title, person_id, color) 
+                                VALUES ($1, $2, $3)`;
+const getCategory = `SELECT DISTINCT category.id, category.title AS title, color.title AS color_title, color.hex_code 
+                    FROM category 
+                    INNER JOIN color
+                    ON color.id = category.color_id
+                    WHERE person_id = $1`;
+const updateCategory = `UPDATE category 
+                        SET title = $1
+                        id_color = $2 
+                        WHERE category.id = $3`;
 
 module.exports = {
   getCategory,
