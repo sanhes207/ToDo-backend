9e3f2dc17e3f6beb7d8a3f86d9046d97fd8c521c
Alexander <amoshenin@mail.ru>
2023-02-22 22:29:28 +0300

    Added a color router to the category path

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,10 +1,13 @@
 const Router = require('express');
-const router = Router();
+const router = new Router();
+const colorRouter = require('./color.routes')
 
 const categoryController = require('../controller/category.controller');
 
 router.post('/category', categoryController.addCategory);
 router.get('/category', categoryController.getCategorys);
 router.put('/category ', categoryController.updateCategory);
+
+router.use('/category', colorRouter);
 
 module.exports = router;
